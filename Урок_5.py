b6ecd1b65d6e1e7f6ff7a60cbbe8f913357727c7
spoliv <spoliv@rambler.ru>
2019-05-21 15:53:37 +0300

    Lesson_5

---
+++ Урок_5.py
@@ -0,0 +1,31 @@
+# 21.05.2019
+
+# Задание №1 (easy)
+
+#import os
+#import sys
+#import shutil
+
+# a)Создайте директорию
+
+#path_dir = [('dir_' + str(i + 1)) for i in range(9)]
+#
+#
+# def make_dir(paths):
+#     dir_path = os.path.join(os.getcwd(), paths)
+#     try:
+#         os.mkdir(dir_path)
+#     except:
+#         print(dir_path + ' - заменить существующую?')
+#
+# for i in path_dir:
+#     make_dir(i)
+
+
+
+
+
+
+
+
+
